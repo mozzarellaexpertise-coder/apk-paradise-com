8737a6f27ff583841e437bbd4700bf4422f45985
GAT <mozzarellaexpertise@gmail.com>
2025-12-29 23:16:47 +0630

    Ronald

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+  // vitePreprocess is faster and preferred for Svelte 5
+  preprocess: vitePreprocess(),
 
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+  kit: {
+    // adapter-auto is only for zero-config platforms; 
+    // adapter-node is correct for Hostinger VPS/Node hosting
+    adapter: adapter({
+      out: 'build', 
+      precompress: true
+    })
+    // REMOVED: target: '#svelte' (This is deprecated and will cause an error)
+  }
 };
 
 export default config;
