ac44bbe86a6df5a7b89188955fd3bb53ade7878b
GAT <mozzarellaexpertise@gmail.com>
2025-12-30 00:00:38 +0630

    Fix SvelteKit Svelte 5 alignment and adapter-node

--- svelte.config.js
+++ svelte.config.js
@@ -2,9 +2,6 @@ import adapter from '@sveltejs/adapter-node';
 
 export default {
   kit: {
-    adapter: adapter({
-      out: 'build',
-      precompress: true
-    })
+    adapter: adapter()
   }
 };
