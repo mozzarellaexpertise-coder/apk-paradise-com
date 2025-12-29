79338e15ca7cd521393fbc22c85ea68e0adea33e
GAT <mozzarellaexpertise@gmail.com>
2025-12-29 23:36:57 +0630

    Fix SvelteKit build for Hostinger

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,10 @@
 import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-  // vitePreprocess is faster and preferred for Svelte 5
-  preprocess: vitePreprocess(),
-
+export default {
   kit: {
-    // adapter-auto is only for zero-config platforms; 
-    // adapter-node is correct for Hostinger VPS/Node hosting
     adapter: adapter({
-      out: 'build', 
+      out: 'build',
       precompress: true
     })
-    // REMOVED: target: '#svelte' (This is deprecated and will cause an error)
   }
 };
-
-export default config;
